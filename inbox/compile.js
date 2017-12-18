df2a01b5bc5319b5ece707506c5d997f9efd9fa2
stephengrider <ste.grider@gmail.com>
2017-12-18 14:39:01 -0800

    036-compile

---
+++ inbox/compile.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
