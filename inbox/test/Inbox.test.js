d8937f41829a128ccaf80fd8a634f1ed4d6e2337
stephengrider <ste.grider@gmail.com>
2017-12-19 15:07:59 -0800

    fetch accounts

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,3 +2,17 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+  // Get a list of all accounts
+  web3.eth.getAccounts().then(fetchedAccounts => {
+    console.log(fetchedAccounts);
+  });
+
+  // Use one of those accounts to deploy
+  // the contract
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {});
+});
