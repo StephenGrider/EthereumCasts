7bb80197403b39f140b420339871bfdfc174ae52
stephengrider <ste.grider@gmail.com>
2017-12-19 15:50:25 -0800

    deployed

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,8 +2,10 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -11,10 +13,13 @@ beforeEach(async () => {
 
   // Use one of those accounts to deploy
   // the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(accounts);
+    console.log(inbox);
   });
 });
