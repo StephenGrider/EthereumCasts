955207f5a1f7adbaee2ee3e5dc3df6603386f43b
stephengrider <ste.grider@gmail.com>
2017-12-19 17:01:36 -0800

    test update

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -14,12 +14,26 @@ beforeEach(async () => {
   // Use one of those accounts to deploy
   // the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })
     .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
   });
 });
