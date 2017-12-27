05ea89ce4976dbd97903b3f3e6c0f338f7a3dafe
stephengrider <ste.grider@gmail.com>
2017-12-27 15:24:25 -0800

    assert deployment

---
+++ lottery/test/Lottery.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const { interface, bytecode } = require('../compile');
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Lottery Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
+});
