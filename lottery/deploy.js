d3579a6b8e3c424c28b29ca5910fa08f64bb8946
stephengrider <ste.grider@gmail.com>
2017-12-28 12:14:41 -0800

    local contract

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -17,6 +17,7 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(interface);
   console.log('Contract deployed to', result.options.address);
 };
 deploy();
