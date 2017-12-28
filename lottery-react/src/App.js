fd125e1ca5a02574f0ea6260ecc0127ef158adb9
stephengrider <ste.grider@gmail.com>
2017-12-28 13:55:46 -0800

    more properties

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
+import lottery from './lottery';
 
 class App extends Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
+  };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {this.state.manager}. There are currently{' '}
+          {this.state.players.length} people entered, competing to win{' '}
+          {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
       </div>
     );
