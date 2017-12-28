1bf179495ce6135f1732767767abd751cc0653e4
stephengrider <ste.grider@gmail.com>
2017-12-28 10:29:33 -0800

    e2e test

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -81,4 +81,18 @@ describe('Lottery Contract', () => {
       assert(err);
     }
   });
+
+  it('sends money to the winner and resets the players array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
+  });
 });
