1d0ef88dae80236790ced1ee56e3d4ce2f097ab5
stephengrider <ste.grider@gmail.com>
2017-12-28 12:15:11 -0800

    local contract

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -5,8 +5,6 @@ import web3 from './web3';
 
 class App extends Component {
   render() {
-    web3.eth.getAccounts().then(console.log);
-
     return (
       <div className="App">
         <header className="App-header">
---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,59 @@
+import web3 from './web3';
+
+const address = '0x8DDB5D5F062C235A9eF6C65b789A3D3951134F0b';
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'pickWinner',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'getPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'enter',
+    outputs: [],
+    payable: true,
+    stateMutability: 'payable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor'
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
