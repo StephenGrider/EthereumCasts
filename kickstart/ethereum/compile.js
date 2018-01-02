f0014f763f86bd5cad6568036df145c08106406b
stephengrider <ste.grider@gmail.com>
2018-01-02 14:53:15 -0800

    more compile

--- kickstart/ethereum/compile.js
+++ kickstart/ethereum/compile.js
@@ -13,7 +13,7 @@ fs.ensureDirSync(buildPath);
 
 for (let contract in output) {
   fs.outputJsonSync(
-    path.resolve(buildPath, contract + '.json'),
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
     output[contract]
   );
 }
