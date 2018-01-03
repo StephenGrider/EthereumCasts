40ea5f60125f57a5f62dc56620011b559739e648
stephengrider <ste.grider@gmail.com>
2018-01-02 16:13:02 -0800

    aray getter

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -61,4 +61,16 @@ describe('Campaigns', () => {
       assert(err);
     }
   });
+
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods
+      .createRequest('Buy batteries', '100', accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+    const request = await campaign.methods.requests(0).call();
+
+    assert.equal('Buy batteries', request.description);
+  });
 });
