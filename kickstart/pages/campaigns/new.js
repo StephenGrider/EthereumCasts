330a2abd36226fbb65f356d2309531a58500a28f
stephengrider <ste.grider@gmail.com>
2018-01-04 14:49:06 -0800

    input change

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: ''
+  };
+
   render() {
     return (
       <Layout>
@@ -11,7 +15,13 @@ class CampaignNew extends Component {
         <Form>
           <Form.Field>
             <label>Minimum Contribution</label>
-            <input />
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event =>
+                this.setState({ minimumContribution: event.target.value })}
+            />
           </Form.Field>
 
           <Button primary>Create!</Button>
