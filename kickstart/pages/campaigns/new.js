5353e504bceecf023d625a43262b1a8e335da95c
stephengrider <ste.grider@gmail.com>
2018-01-04 14:41:15 -0800

    added form

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Form, Button } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
   render() {
     return (
       <Layout>
-        <h1>New Campaign!</h1>
+        <h3>Create a Campaign</h3>
+
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <input />
+          </Form.Field>
+
+          <Button primary>Create!</Button>
+        </Form>
       </Layout>
     );
   }
