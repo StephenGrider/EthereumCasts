45c0037e8a2801283bcd87b2c4e652824b71d07d
stephengrider <ste.grider@gmail.com>
2018-01-08 14:24:15 -0800

    nav

--- kickstart/components/Header.js
+++ kickstart/components/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
   return (
     <Menu style={{ marginTop: '10px' }}>
-      <Menu.Item>CrowdCoin</Menu.Item>
+      <Link route="/">
+        <a className="item">CrowdCoin</a>
+      </Link>
 
       <Menu.Menu position="right">
-        <Menu.Item>Campaigns</Menu.Item>
+        <Link route="/">
+          <a className="item">Campaigns</a>
+        </Link>
 
-        <Menu.Item>+</Menu.Item>
+        <Link route="/campaigns/new">
+          <a className="item">+</a>
+        </Link>
       </Menu.Menu>
     </Menu>
   );
--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -3,6 +3,7 @@ import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import { Router } from '../../routes';
 
 class CampaignNew extends Component {
   state = {
@@ -23,6 +24,8 @@ class CampaignNew extends Component {
         .send({
           from: accounts[0]
         });
+
+      Router.pushRoute('/');
     } catch (err) {
       this.setState({ errorMessage: err.message });
     }
