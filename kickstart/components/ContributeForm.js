911736aa98e415cafaf1429a403b014c62a5dbcf
stephengrider <ste.grider@gmail.com>
2018-01-10 12:28:29 -0800

    cont

--- kickstart/components/ContributeForm.js
+++ kickstart/components/ContributeForm.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { Form, Input, Message, Button } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
 
 class ContributeForm extends Component {
   state = {
     value: ''
   };
 
-  onSubmit = event => {
+  onSubmit = async event => {
     event.preventDefault();
 
     const campaign = Campaign(this.props.address);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether')
+      });
+    } catch (err) {}
   };
 
   render() {
