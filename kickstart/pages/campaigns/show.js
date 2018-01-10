d13ca26574fc409bbc02aaa4bdbd0d957ded348c
stephengrider <ste.grider@gmail.com>
2018-01-10 11:26:35 -0800

    more cards

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -34,6 +35,30 @@ class CampaignShow extends Component {
         description:
           'The manager created this campaign and can create requests to withdraw money',
         style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (wei)',
+        description:
+          'You must contribute at least this much wei to become an approver'
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of Requests',
+        description:
+          'A request tries to withdraw money from the contract. Requests must be approved by approvers'
+      },
+      {
+        header: approversCount,
+        meta: 'Number of Approvers',
+        description:
+          'Number of people who have already donated to this campaign'
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign Balance (ether)',
+        description:
+          'The balance is how much money this campaign has left to spend.'
       }
     ];
 
