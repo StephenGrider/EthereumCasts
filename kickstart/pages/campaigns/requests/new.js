a97c3dee1aab75fb0e722b39ed92bdf2e44f17fc
stephengrider <ste.grider@gmail.com>
2018-01-10 14:56:37 -0800

    request

--- kickstart/pages/campaigns/requests/new.js
+++ kickstart/pages/campaigns/requests/new.js
@@ -1,8 +1,58 @@
 import React, { Component } from 'react';
+import { Form, Button, Message, Input } from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+import { Link, Router } from '../../../routes';
+import Layout from '../../../components/Layout';
 
 class RequestNew extends Component {
+  state = {
+    value: '',
+    description: '',
+    recipient: ''
+  };
+
+  static async getInitialProps(props) {
+    const { address } = props.query;
+
+    return { address };
+  }
+
   render() {
-    return <h3>Create a Request</h3>;
+    return (
+      <Layout>
+        <h3>Create a Request</h3>
+        <Form>
+          <Form.Field>
+            <label>Description</label>
+            <Input
+              value={this.state.description}
+              onChange={event =>
+                this.setState({ description: event.target.value })}
+            />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Value in Ether</label>
+            <Input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Recipient</label>
+            <Input
+              value={this.state.recipient}
+              onChange={event =>
+                this.setState({ recipient: event.target.value })}
+            />
+          </Form.Field>
+
+          <Button primary>Create!</Button>
+        </Form>
+      </Layout>
+    );
   }
 }
 
