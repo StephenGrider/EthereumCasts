58125e5a5f92ff84d299b4d88f7ca9e9ae0fb473
stephengrider <ste.grider@gmail.com>
2018-01-10 13:37:33 -0800

    refresh

--- kickstart/components/ContributeForm.js
+++ kickstart/components/ContributeForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Form, Input, Message, Button } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
 import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributeForm extends Component {
   state = {
@@ -19,6 +20,8 @@ class ContributeForm extends Component {
         from: accounts[0],
         value: web3.utils.toWei(this.state.value, 'ether')
       });
+
+      Router.replaceRoute(`/campaigns/${this.props.address}`);
     } catch (err) {}
   };
 
