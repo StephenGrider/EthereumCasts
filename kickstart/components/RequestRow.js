bed533f0bc0dbd5cdfe69bb4600a68618ecaf4a3
stephengrider <ste.grider@gmail.com>
2018-01-15 14:34:28 -0800

    request row

---
+++ kickstart/components/RequestRow.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class RequestRow extends Component {
+  render() {
+    return <div>Request!</div>;
+  }
+}
+
+export default RequestRow;
--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
@@ -18,12 +19,24 @@ class RequestIndex extends Component {
         })
     );
 
-    console.log(requests);
+    return { address, requests, requestCount };
+  }
 
-    return { address };
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          request={request}
+          address={this.props.address}
+        />
+      );
+    });
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <h3>Requests</h3>
@@ -32,6 +45,20 @@ class RequestIndex extends Component {
             <Button primary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+        </Table>
       </Layout>
     );
   }
