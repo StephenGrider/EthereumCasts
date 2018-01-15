9e078166169a5ebd9850d3d683fc3639fab6ba97
stephengrider <ste.grider@gmail.com>
2018-01-15 14:09:39 -0800

    fancy js

--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    const requests = await Promise.all(
+      Array(requestCount)
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+
+    console.log(requests);
 
     return { address };
   }
