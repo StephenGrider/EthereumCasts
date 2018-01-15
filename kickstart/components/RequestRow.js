15875a8fcb5d7f6434f90d0fc96d9767331ff7a5
stephengrider <ste.grider@gmail.com>
2018-01-15 15:19:21 -0800

    finalize request

--- kickstart/components/RequestRow.js
+++ kickstart/components/RequestRow.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
+import Campaign from '../ethereum/campaign';
 
 class RequestRow extends Component {
+  onApprove = async () => {
+    const campaign = Campaign(this.props.address);
+
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.approveRequest(this.props.id).send({
+      from: accounts[0]
+    });
+  };
+
+  onFinalize = async () => {
+    const campaign = Campaign(this.props.address);
+
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.finalizeRequest(this.props.id).send({
+      from: accounts[0]
+    });
+  };
+
   render() {
     const { Row, Cell } = Table;
     const { id, request, approversCount } = this.props;
@@ -15,6 +34,16 @@ class RequestRow extends Component {
         <Cell>{request.recipient}</Cell>
         <Cell>
           {request.approvalCount}/{approversCount}
+        </Cell>
+        <Cell>
+          <Button color="green" basic onClick={this.onApprove}>
+            Approve
+          </Button>
+        </Cell>
+        <Cell>
+          <Button color="teal" basic onClick={this.onFinalize}>
+            Finalize
+          </Button>
         </Cell>
       </Row>
     );
