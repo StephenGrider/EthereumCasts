9323f0ed63566acaa2bc466356d86e5fc6b48f7b
stephengrider <ste.grider@gmail.com>
2018-01-15 15:57:42 -0800

    row status

--- kickstart/components/RequestRow.js
+++ kickstart/components/RequestRow.js
@@ -25,9 +25,13 @@ class RequestRow extends Component {
   render() {
     const { Row, Cell } = Table;
     const { id, request, approversCount } = this.props;
+    const readyToFinalize = request.approvalCount > approversCount / 2;
 
     return (
-      <Row>
+      <Row
+        disabled={request.complete}
+        positive={readyToFinalize && !request.complete}
+      >
         <Cell>{id}</Cell>
         <Cell>{request.description}</Cell>
         <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
@@ -36,14 +40,18 @@ class RequestRow extends Component {
           {request.approvalCount}/{approversCount}
         </Cell>
         <Cell>
-          <Button color="green" basic onClick={this.onApprove}>
-            Approve
-          </Button>
+          {request.complete ? null : (
+            <Button color="green" basic onClick={this.onApprove}>
+              Approve
+            </Button>
+          )}
         </Cell>
         <Cell>
-          <Button color="teal" basic onClick={this.onFinalize}>
-            Finalize
-          </Button>
+          {request.complete ? null : (
+            <Button color="teal" basic onClick={this.onFinalize}>
+              Finalize
+            </Button>
+          )}
         </Cell>
       </Row>
     );
--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -13,7 +13,7 @@ class RequestIndex extends Component {
     const approversCount = await campaign.methods.approversCount().call();
 
     const requests = await Promise.all(
-      Array(requestCount)
+      Array(parseInt(requestCount))
         .fill()
         .map((element, index) => {
           return campaign.methods.requests(index).call();
