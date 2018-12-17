48534abb0454e0745b5c52d97e81bb0a6630c138
Brooks.ideas <45931201+brooksideas@users.noreply.github.com>
2018-12-17 11:38:36 +0300

    MaxListenerExceededWarning fixed!
    Possible EventEmitter memory leak detected.  27 data listeners are added therefore we can adjust defaultMaxListeners to increase the limit.

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -3,6 +3,9 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+///MaxListenerExceededWarning fixed!
+require('events').EventEmitter.defaultMaxListeners = 27;
+
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
 
