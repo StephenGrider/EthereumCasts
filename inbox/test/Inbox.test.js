41d54349c87f8da6d0fad67d90242b3d2602225e
Brooks.ideas <45931201+brooksideas@users.noreply.github.com>
2018-12-17 11:19:12 +0300

    MaxListenerExceededWarning fixed!
    Possible EventEmitter memory leak detected. 14 data listeners are added therefore we can use the emitter.setMaxListener() to increase the limit.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,6 +2,12 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+//to handle memory leak
+require('events').EventEmitter.defaultMaxListeners = 15;
+
+
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
