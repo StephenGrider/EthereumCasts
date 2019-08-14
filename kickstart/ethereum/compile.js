5d22e4b23e984477087101c60ec62e54edcd5bfd
Dinslinger90 <53660883+Dinslinger90@users.noreply.github.com>
2019-08-14 20:42:18 +0530

    Updated to current implementaion standard
    Changed the implementation standard to currently working one.

--- kickstart/ethereum/compile.js
+++ kickstart/ethereum/compile.js
@@ -7,13 +7,31 @@ fs.removeSync(buildPath);
 
 const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 const source = fs.readFileSync(campaignPath, 'utf8');
-const output = solc.compile(source, 1).contracts;
+/// @dev This works currently.
+const input = {
+	    language: 'Solidity',
+	    sources: {
+	        'Campaign.sol' : {
+	            content: source
+	        }
+	    },
+	    settings: {
+	        outputSelection: {
+	            '*': {
+	                '*': [ '*' ]
+	            }
+	        }
+	    }
+	};
+/// @dev Gotta change this either.
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts;
 
 fs.ensureDirSync(buildPath);
 
+/// @notice This works.
 for (let contract in output) {
-  fs.outputJsonSync(
-    path.resolve(buildPath, contract.replace(':', '') + '.json'),
-    output[contract]
-  );
-}
+	  	fs.outputJsonSync(
+		    path.resolve(buildPath, contract.replace('.sol', '') + '.json'),
+		    output[contract]
+	  	);
+	}
