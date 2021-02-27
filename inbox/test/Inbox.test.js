5cf6d4042cd8eea00feab002c8f6e91f8027cec4
Ivan Damyanov <iidamyanov@yahoo.com>
2021-02-27 22:08:46 +0100

    updated Inbox section to newer packages and libraries and added a README to help use .env with Metamask for infura

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,38 +2,32 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
-
+const abi = require('../compile').abi; // the contract interface aka ABI
+const bytecode = require('../compile').evm.bytecode.object
 let accounts;
 let inbox;
 
 beforeEach(async () => {
-  // Get a list of all accounts
-  accounts = await web3.eth.getAccounts();
+    accounts = await web3.eth.getAccounts();
 
-  // Use one of those accounts to deploy
-  // the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-      arguments: ['Hi there!']
-    })
+    inbox = await new web3.eth.Contract(abi)
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {
-    assert.ok(inbox.options.address);
-  });
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address); // if address is there, we are confident deployment succeeded
+    });
 
-  it('has a default message', async () => {
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'Hi there!');
-  });
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, 'Hi there!');
+    });
 
-  it('can change the message', async () => {
-    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'bye');
-  });
+    it('can change message', async () => {
+        res = await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, 'bye');
+    })
 });
